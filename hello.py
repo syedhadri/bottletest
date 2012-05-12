3771aaf22893fce6756f8f26aff8876158b67363
Syed Mohd 'Aizat Hadri Bin Syed Salleh <syedhadri@gmail.com>
2012-05-12 23:33:47 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,8 @@ import bottle
 @bottle.route('/', method = 'GET', template = 'hello')
 @bottle.route('/<name>', method = 'GET', template = 'hello')
 def hello(name = 'world'):
-    session = bottle.request.environ.get('beaker.session')
-    session['__session__'] = 'smah'
-    session.save()
-    return {'name': name,'counter': session['__session__']}
+    #session = bottle.request.environ.get('beaker.session')
+    #session['__session__'] = 'smah'
+    #session.save()
+    #return {'name': name,'counter': session['__session__']}
+    return {'name': name}
